bf2c02627563050f3157d229b05b6eb5bfed1618
Marek Szuba <mszuba01@gmail.com>
2020-09-05 21:13:28 +0200

    add useBookSearch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useBookSearch from './useBookSearch'
+
 
 
 export default function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState('')
+
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+
+
+  useBookSearch(query, pageNumber)
   return (
     <>
-      <input type="text" />
+      <input type="text" onChange={handleSearch} />
       <div>Title</div>
       <div>Title</div>
       <div>Title</div>
