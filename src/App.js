371008c3f3125eb322485f0a3c93db9564f69859
Marek Szuba <mszuba01@gmail.com>
2020-09-06 19:30:41 +0200

    add loading feature

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import useBookSearch from './useBookSearch'
 
 
 
 export default function App() {
   const [query, setQuery] = useState('')
-  const [pageNumber, setPageNumber] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
 
+  const {
+    books,
+    hasMore,
+    loading,
+    error
+  } = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
 
   function handleSearch(e) {
     setQuery(e.target.value)
     setPageNumber(1)
   }
 
-
-
-  useBookSearch(query, pageNumber)
   return (
     <>
-      <input type="text" onChange={handleSearch} />
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Loading...</div>
-      <div>Error</div>
+      <input type="text" value={query} onChange={handleSearch} />
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        } else {
+          return <div key={book}>{book}</div>
+        }
+        return
+      })}
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
     </>
   );
 };
--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -7,8 +7,13 @@ export default function useBookSearch(query, pageNumber) {
   const [books, setBooks] = useState([])
   const [hasMore, setHasMore] = useState(false)
 
+  useEffect(() => {
+    setBooks([])
+  }, [query])
 
   useEffect(() => {
+    setLoading(true)
+    setError(false)
     let cancel
     axios({
       method: 'GET',
@@ -16,11 +21,18 @@ export default function useBookSearch(query, pageNumber) {
       params: { q: query, page: pageNumber },
       cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
+      setBooks(prevBooks => {
+        return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+      })
+      setHasMore(res.data.docs.length > 0)
+      setLoading(false)
       console.log(res.data);
     }).catch(e => {
       if (axios.isCancel(e)) return
+      setError(true)
     })
     return () => cancel()
   }, [query, pageNumber])
-  return null
+
+  return { loading, error, books, hasMore }
 }
